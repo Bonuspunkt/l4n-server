274d5a2c58984af0490fb754da7fe5d11d73de6a
Bonuspunkt <bonuspunkt+github@gmail.com>
2017-08-19 00:00:55 +0200

    step

--- settings.sample.js
+++ settings.sample.js
@@ -1,9 +1,13 @@
 module.exports = {
-    // must be a https url for battlenet
+    // must be a https url for battle.net
+    // NOTE: we only provide a HTTP server - use reverse proxy infront
     hostUrl: 'https://myDomain.com/',
     session: {
-        // v-- replace
+        // https://www.npmjs.com/package/express-session
         secret: 'the cat runs over the keyboard'
+    },
+    sessionStore: {
+        // https://www.npmjs.com/package/connect-sqlite3
     },
     steam: {
         // get it from https://steamcommunity.com/dev/apikey
