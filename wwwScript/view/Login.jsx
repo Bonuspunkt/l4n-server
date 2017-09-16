d4560ca30cbb5b986d698e8c09017fa7b93f7762
Bonuspunkt <bonuspunkt+github@gmail.com>
2017-09-16 14:37:56 +0200

    style login

--- wwwScript/view/Login.jsx
+++ wwwScript/view/Login.jsx
@@ -17,16 +17,22 @@ const Login = props => {
             <h3>sign in</h3>
             <form action="/login" method="POST">
                 <CsrfToken {...props} />
-                <label>
-                    <span>Username</span>
-                    <input type="text" name="username" required autoFocus />
+                <label className="formField">
+                    <span className="formField-label">Username</span>
+                    <input
+                        className="formField-input"
+                        type="text"
+                        name="username"
+                        required
+                        autoFocus
+                    />
                 </label>
-                <label>
-                    <span>Password</span>
-                    <input type="password" name="password" required />
+                <label className="formField">
+                    <span className="formField-label">Password</span>
+                    <input className="formField-input" type="password" name="password" required />
                 </label>
-                <label>
-                    <span />
+                <label className="fieldName">
+                    <span className="formField-label" />
                     <button type="submit">Login</button>
                 </label>
             </form>
