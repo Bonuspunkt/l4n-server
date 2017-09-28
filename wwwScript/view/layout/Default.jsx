3b26976204d839c227d321e877e578b852ac4732
Bonuspunkt <bonuspunkt+github@gmail.com>
2017-09-28 23:08:30 +0200

    fix display html as utf8

--- wwwScript/view/layout/Default.jsx
+++ wwwScript/view/layout/Default.jsx
@@ -26,6 +26,7 @@ const DefaultLayout = props => {
             <head>
                 <title>{usedTitle}</title>
                 <link rel="stylesheet" href="/stylesheet.css" type="text/css" />
+                <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </head>
             <body>
